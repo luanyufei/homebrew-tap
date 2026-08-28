d8ba6579847b5312310317e5c8b4337f8f57f2be
luanyufei <luanyufei666@outlook.com>
2026-08-28 23:11:14 +0800

    fix(formula): update sha256 for dwum v0.1.0 static build

--- Formula/dwum.rb
+++ Formula/dwum.rb
@@ -2,7 +2,7 @@ class Dwum < Formula
   desc "Delete archives while unzipping with APFS zero-write hole punching"
   homepage "https://github.com/luanyufei/delete_when_unzip_mac"
   url "https://github.com/luanyufei/delete_when_unzip_mac/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6c91b1d89bc372366d4847d5c9f60d73edff3f746336e4a4c3941e12ae7725ae"
+  sha256 "c831cf40fc548f641254be28e0237f2427f32da560b9ffd0f5bb393833c7f285"
   license "Apache-2.0"
 
   depends_on "libarchive"
