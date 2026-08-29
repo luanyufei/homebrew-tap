df498f84a4243e9d6c8155891d9e3b272fca7035
luanyufei <luanyufei666@outlook.com>
2026-08-29 12:19:43 +0800

    release: update formula to DeleteWhenUnzipMac v0.1.5

--- Formula/dwum.rb
+++ Formula/dwum.rb
@@ -1,8 +1,8 @@
 class Dwum < Formula
   desc "Delete archives while unzipping with APFS zero-write hole punching"
   homepage "https://github.com/luanyufei/delete_when_unzip_mac"
-  url "https://github.com/luanyufei/delete_when_unzip_mac/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "bc4502f13f5f32d6cb0549d3a9c8e3c8d9ebe864c94581517d659d73707abc83"
+  url "https://github.com/luanyufei/delete_when_unzip_mac/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "86276c916a9c82d8489ccdf728418c6d91ea992b93a7e33a1f7bf7dca60c78c0"
   license "Apache-2.0"
 
   depends_on "libarchive"
