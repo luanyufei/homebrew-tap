6abb16052b677b0bac9dd8381829ff0ce3a66b29
luanyufei <79290667+luanyufei@users.noreply.github.com>
2026-08-30 18:52:33 +0800

    dwum 0.2.0

--- Formula/dwum.rb
+++ Formula/dwum.rb
@@ -1,8 +1,8 @@
 class Dwum < Formula
   desc "Delete archives while unzipping with APFS zero-write hole punching"
   homepage "https://github.com/luanyufei/delete_when_unzip_mac"
-  url "https://github.com/luanyufei/delete_when_unzip_mac/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "da5cc1bbed46b07f4d05e948b7d22de1653d62a97939a900d042821f45520b19"
+  url "https://github.com/luanyufei/delete_when_unzip_mac/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "3e38137239e87a33dc52575497c8bfe24f4f3dde2070c8a414c2d13293a9ea37"
   license "Apache-2.0"
 
   depends_on "libarchive"
